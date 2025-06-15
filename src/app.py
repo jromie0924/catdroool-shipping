60dc590d93aae668ec82c4d97d5096ab8b75a491
Jackson Romie <jromie0924@gmail.com>
2025-06-15 10:11:44 -0500

    #15 sort lists by city alphabetically (#16)
    * add sql file for making modifications to database manually
    
    * update gitignore
    
    * update logging
    
    * add sorting functionality

--- src/app.py
+++ src/app.py
@@ -8,12 +8,17 @@ from services.aws import Aws
 from services.catdroool import Catdroool
 from config import config
 
+now = dt.now()
+
 def _init_logger():
   logger = logging.getLogger(config.APP_NAME)
   logger.setLevel(config.LOGGING_LEVEL)
   
+  now_str = now.strftime(config.DATE_FORMAT_STRING)
+  log_file_path = f"{config.LOG_FILE_LOCATION}/{now_str}/catdroool.log"
+  
   # Create log directory if it doesn't exist
-  dir = os.path.dirname(config.LOG_FILE)
+  dir = os.path.dirname(log_file_path)
   os.makedirs(dir, exist_ok=True)
   
   # Log formatter
@@ -27,7 +32,7 @@ def _init_logger():
   # Will keep 2 files, each with a max size of 1MB
   # When the log file reaches 1MB, it will create a new log file
   # The oldest log file will be deleted
-  file_handler = RotatingFileHandler(config.LOG_FILE, mode='a', maxBytes=1024*1024, backupCount=2)
+  file_handler = RotatingFileHandler(log_file_path, mode='a', maxBytes=1024*1024*10, backupCount=1)
   file_handler.setFormatter(formatter)
   
   # Add handlers to the logger
@@ -44,5 +49,5 @@ if __name__ == '__main__':
     logger.error(f"Error: {e}")
     sys.exit(1)
   
-  catdroool = Catdroool()
+  catdroool = Catdroool(now=now)
   catdroool.generate_report()
